2edf1e68d0f289520a485fef7c80bac249741551
vynfill <erlangzdcs@gmail.com>
2024-12-18 11:08:41 +0700

    Add files via upload

---
+++ views/site/contacts.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = '';
+?>
+
+<div class="site-contacts">
+    <h1><?= \yii\helpers\Html::encode($this->title) ?></h1>
+
+    <p>Untuk detail lebih lanjut silahkan kontak kami via WhatsApp:</p>
+
+    <div class="whatsapp-link">
+        <!-- WhatsApp Link to open chat -->
+        <a href="https://wa.me/6285219902699?text=Saya%20tertarik%20dengan%20produk%20yangr%20anda%20tawarkan." 
+           target="_blank" class="btn btn-success">Klik disini 👇</a>
+    </div>
+</div>
